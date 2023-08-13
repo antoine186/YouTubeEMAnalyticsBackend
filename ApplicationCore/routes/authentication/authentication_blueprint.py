ac2bb24ef591ff28040eb47b5f7369b30ed68b55
Antoine Yi-Cheng Tian <33133769+antoine186@users.noreply.github.com>
2023-08-13 20:53:41 +0200

    Fixed incorrect login out when cookies disappear

--- ApplicationCore/routes/authentication/authentication_blueprint.py
+++ ApplicationCore/routes/authentication/authentication_blueprint.py
@@ -57,6 +57,7 @@ def login():
             response = make_response(json.dumps(operation_response))
             #response.set_cookie('user_session_cookie', json.dumps(session_token), max_age = seconds, samesite = None , secure = True, httponly = True)
             response.set_cookie('user_session_cookie', json.dumps(session_token), max_age = seconds, samesite = None , secure = False, httponly = False)
+            #response.set_cookie('user_session_cookie', json.dumps(session_token), samesite = None , secure = False, httponly = False)
             # response.headers['Access-Control-Allow-Origin'] = 'http://localhost/'
             # response.headers['Access-Control-Allow-Origin'] = '*'
             # response.headers['Access-Control-Allow-Credentials'] = 'true'
