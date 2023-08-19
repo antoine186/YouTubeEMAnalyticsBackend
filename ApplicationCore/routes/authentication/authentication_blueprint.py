f082e28d65309a424a2e06b65e1178ac026bc027
Antoine Yi-Cheng Tian <33133769+antoine186@users.noreply.github.com>
2023-08-19 21:02:39 +0200

    Debugging CORS

--- ApplicationCore/routes/authentication/authentication_blueprint.py
+++ ApplicationCore/routes/authentication/authentication_blueprint.py
@@ -59,7 +59,7 @@ def login():
             response.set_cookie('user_session_cookie', json.dumps(session_token), max_age = seconds, samesite = None , secure = False, httponly = False)
             #response.set_cookie('user_session_cookie', json.dumps(session_token), samesite = None , secure = False, httponly = False)
             # response.headers['Access-Control-Allow-Origin'] = 'http://localhost/'
-            # response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Origin'] = '*'
             # response.headers['Access-Control-Allow-Credentials'] = 'true'
 
         else:
@@ -70,7 +70,8 @@ def login():
                 "error_message": "one_session_already_active"
             }
             response = make_response(json.dumps(operation_response))
-
+        
+        response.headers['Access-Control-Allow-Origin'] = '*'
         return response
 
     operation_response = {
@@ -81,4 +82,5 @@ def login():
     }
     response = make_response(json.dumps(operation_response))
 
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
